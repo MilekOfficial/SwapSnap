dd1751988c5b917346b43b9c1a2eb9c34fc34f99
MilekOfficial <studiomilek@gmail.com>
2024-11-16 13:07:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -131,4 +131,4 @@ def react():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000)
